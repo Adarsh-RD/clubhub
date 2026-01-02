de0e8e7de9297da49e095372e462e55ba372130f
adarshdodamania-ship-it <adarshdodamania@gmail.com>
2026-01-02 18:42:46 +0530

    Switch to connection string for DB

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || "postgres",
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
-  family: 4 // Force IPv4 to resolve Render ENETUNREACH errors
+  connectionString: `postgres://${process.env.DB_USER || "postgres"}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME || "postgres"}?sslmode=no-verify`,
+  ssl: { rejectUnauthorized: false },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection could not be established
 });
 
 /* =========================
